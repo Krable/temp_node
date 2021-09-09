6b43474ad57e5ceea24b7368349c3aeec6773041
Lucas Lemoine <lemoinelucas1@gmail.com>
2021-09-09 19:32:44 +0200

    first commit

---
+++ 01-intro.js
@@ -0,0 +1,8 @@
+const amout = 9
+
+if(amout < 9){
+    console.log("Le nombre est petit")
+}else{
+    console.log("Le nombre est grand")
+}
+console.log("C'est ma première application en nodeJS")
---
+++ 10-fs-sync.js
@@ -0,0 +1,16 @@
+const { readFileSync, writeFileSync } = require('fs');
+console.log('Début');
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+console.log(first, second);
+
+writeFileSync(
+    './content/result-sync.txt', 
+    `Here is the result : ${first}, ${second}`,
+    {flag: 'a'}
+);
+
+console.log('La tâche est finie');
+console.log('Démarrage de la prochaine Tâche');
+
